6ad2f39c66131d220589567fdd2c9b2186e02ebc
rbong <r.l.bongers@gmail.com>
2016-02-25 08:34:58 -0500

    Added plug-in to hide all layers.

---
+++ hide_unhide.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from gimpfu import *
+
+def hide_all_layers (image):
+    layers = image.layers
+    nlayers = len (layers)
+    i = 0
+    while i < nlayers:
+        pdb.gimp_item_set_visible (layers [i], 0)
+        i += 1
+    pdb.gimp_displays_flush ()
+
+register(
+    "hide_all_layers",
+    "hide_all_layers",
+    "Hide all layers",
+    "Roger Bongers",
+    "Roger Bongers",
+    "2016",
+    "Hide all Layers",
+    "*",
+    [
+        (PF_IMAGE, "image", "The image to modify", None),
+    ],
+    [],
+    hide_all_layers, menu="<Image>/Layer")
+
+main()
