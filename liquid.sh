223bc8bd5534b6280bcf388a04441dfb158c79b9
rbong <r.l.bongers@gmail.com>
2019-01-23 17:13:24 -0500

    Clean scripts

--- liquid.sh
+++ liquid.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 filename=$1
-percent=$2
-nrg_func=$3
-if [ "$2" == "" ]; then percent='0.01'; fi
-if [ "$3" == "" ]; then nrg_func='0'; fi
-gimp -i -b '(python-fu-batch-liquify RUN-NONINTERACTIVE '\"$filename\"' '$percent' '$nrg_func')' -b '(gimp-quit 0)'
+percent=${2:-0.01}
+nrg_func=${3:-0}
+gimp -i \
+  -b '(python-fu-batch-liquify RUN-NONINTERACTIVE "'"$filename"'" '$percent' '$nrg_func')' \
+  -b '(gimp-quit 0)'
--- rainbow.sh
+++ rainbow.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 filename=$1
-loops=$2
-frequency=$3
-if [ "$2" == "" ]; then loops=1; fi
-if [ "$3" == "" ]; then frequency='1.2'; fi
-gimp -i -b '(python-fu-batch-rainbowfy RUN-NONINTERACTIVE '\"$filename\"' '$loops' '$frequency')' -b '(gimp-quit 0)'
+loops=${2:-1}
+frequency=${3:-1.2}
+gimp -i \
+  -b '(python-fu-batch-rainbowfy RUN-NONINTERACTIVE "'"$filename"'" '$loops' '$frequency')' \
+  -b '(gimp-quit 0)'
